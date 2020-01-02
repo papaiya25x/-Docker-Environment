9c79f44add8e78111d5caa58b54f3e4702a019e2
阿部秀哉 <shuya@abeshuusainoMacBook-puro.local>
2020-01-02 23:19:02 +0900

    first commit

---
+++ docker-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+# aptパッケージの更新
+sudo apt-get update
+
+# パッケージをインストール
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+# Dockerの公式GPGキーを追加
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# 安定したリポジトリを設定
+sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
+
+# aptパッケージの更新
+sudo apt-get update
+
+# Dockerのインストール
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# docker-composeのインストール
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
